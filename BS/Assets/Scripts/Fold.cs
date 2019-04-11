d8ac5ade8005ffd8667c14f0a2874c70eccf5e01
Huw Messie <hmessie@andrew.cmu.edu>
2019-04-11 15:51:49 -0400

    Folding paper with pencil

---
+++ BS/Assets/Scripts/Fold.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fold : MonoBehaviour
+{
+    float mouseXPrev;
+    float mouseXCur;
+    float dist;
+    float ori = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseXCur = Input.mousePosition.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseXPrev = mouseXCur;
+        mouseXCur = Input.mousePosition.x;
+        dist = mouseXCur - mouseXPrev;
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 rot = new Vector3(0, 0, -dist);
+            ori += dist;
+            if (ori > 0 && ori < 180)
+            {
+                this.transform.Rotate(rot);
+            }
+            else ori -= dist;
+        }
+    }
+}
